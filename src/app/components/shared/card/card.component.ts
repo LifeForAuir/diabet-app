37c9e07c131cbf677a3d8d83bb17a488b654d613
dudorov <dudorov@elma-bpm.com>
2025-03-05 21:59:20 +0400

    Показать еще в карточке

--- src/app/components/shared/card/card.component.ts
+++ src/app/components/shared/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, TemplateRef, ContentChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-card',
@@ -11,12 +12,34 @@ import { MatListModule } from '@angular/material/list';
   imports: [
     CommonModule,
     MatCardModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class CardComponent<T> {
   @Input() title: string = '';
   @Input() contentClass: string = '';
   @Input() items: T[] = [];
+  @Input() readonly itemsPerPage = 3;
   @ContentChild('itemTemplate') itemTemplate!: TemplateRef<{ $implicit: T }>;
+
+  visibleItems: T[] = [];
+  private currentPage = 1;
+
+  ngOnChanges(): void {
+    this.updateVisibleItems();
+  }
+
+  loadMore(): void {
+    this.currentPage++;
+    this.updateVisibleItems();
+  }
+
+  private updateVisibleItems(): void {
+    this.visibleItems = this.items.slice(0, this.itemsPerPage * this.currentPage);
+  }
+
+  get hasMoreItems(): boolean {
+    return this.visibleItems.length < this.items.length;
+  }
 }
